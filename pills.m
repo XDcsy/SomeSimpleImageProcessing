b4143f47ac7ee82e39b4ec74d5d56d3e166c223e
neEverett <shengyang.cheng@outlook.com>
2017-04-23 23:08:43 +0800

    Add files via upload

--- pills.m
+++ pills.m
@@ -9,4 +9,9 @@ se=strel('disk',5);
 bw = imopen(bw, se);
 gray = uint8(bw).*gray;
 %result 1
-rect = minboundrect(c,r,'a');
+e = edge(bw,'canny');
+theta = 1:180;
+[R,xp] = radon(e,theta);
+[I,J] = find(R>=max(max(R)));%J记录了倾斜角，最大的倾斜角
+angle=90-J;
+bw = imrotate(bw,angle,'bicubic','crop');
